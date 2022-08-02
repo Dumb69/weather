5b2770bfb639c0e7d046ecc4b56489a1a204631b
KANISHKA DHADA <56750095+Dumb69@users.noreply.github.com>
2022-08-02 09:16:47 +0530

    Add files via upload

---
+++ weathapp/weath.js
@@ -0,0 +1,34 @@
+let weather = {
+    "apikey": "591ad0d1720b34c6fe81e04d5e95e67c",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apikey
+        )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "Km/hr";
+
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".searchbar").value);
+    },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+})
